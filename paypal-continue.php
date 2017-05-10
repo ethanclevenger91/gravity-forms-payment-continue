c7b332d1d44c7b5d63a798ac26a1078fa5805207
eclev91 <eclev91@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-05-10 15:37:52 +0000

    Added functin to access plugin instance
    git-svn-id: https://plugins.svn.wordpress.org/gf-payment-continue/trunk@1654434 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- paypal-continue.php
+++ paypal-continue.php
@@ -6,25 +6,46 @@ Version: 1.0
 Author: Sterner Stuff Design
 Author URI: https://sternerstuffdesign.com
 */
+
 define( 'GF_PAYPAL_CONTINUE_ADDON_VERSION', '1.0' );
 
+// If Gravity Forms is loaded, bootstrap Gravity Forms PayPal Standard Continue.
 add_action( 'gform_loaded', array( 'GF_PayPal_Continue_AddOn_Bootstrap', 'load' ), 5 );
 
+/**
+ * Class GF_PayPal_Continue_AddOn_Bootstrap
+ *
+ * Handles the loading of Gravity Forms PayPal Standard Continue and registers with the Add-On framework.
+ */
 class GF_PayPal_Continue_AddOn_Bootstrap {
 
-    public static function load() {
+	/**
+	 * If the Add-On Framework exists, Gravity Forms PayPal Standard Continue is loaded.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function load() {
 
-        if ( ! method_exists( 'GFForms', 'include_addon_framework' ) ) {
-            return;
-        }
+		if ( ! method_exists( 'GFForms', 'include_addon_framework' ) ) {
+			return;
+		}
 
-        require_once( 'gravityforms-paypalcontinue.php' );
+		require_once( 'gravityforms-paypalcontinue.php' );
 
-        GFAddOn::register( 'GravityFormsPayPalContinue' );
-    }
+		GFAddOn::register( 'GravityFormsPayPalContinue' );
+
+	}
 
 }
 
-function gf_simple_addon() {
-    return GFSimpleAddOn::get_instance();
+/**
+ * Returns an instance of the GravityFormsPayPalContinue class
+ *
+ * @see	   GravityFormsPayPalContinue::get_instance()
+ *
+ * @return object GravityFormsPayPalContinue
+ */
+function gf_paypal_continue() {
+	return GravityFormsPayPalContinue::get_instance();
 }
